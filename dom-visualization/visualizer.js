b6584634af306b26dcf7db50e2dc553e7e22f3b7
snakazawa <nakazawa.tms0314@gmail.com>
2014-06-17 13:07:31 +0900

    ルートディレクトリ追加、line追加

--- dom-visualization/visualizer.js
+++ dom-visualization/visualizer.js
@@ -12,9 +12,12 @@ var DOM_VISUALIZER = DOM_VISUALIZER || {};
             svg = d3_svg,
             w = svg.attr('width'),
             h = svg.attr('height'),
-            dom = $(),
+            dom,
             edges = [],
-            nodes = [];
+            nodes = [],
+            min_margin_horizontal = 30,
+            min_margin_vertical = 30,
+            truncate_text_length = 5;
 
         function compute_positions() {
 
@@ -27,32 +30,60 @@ var DOM_VISUALIZER = DOM_VISUALIZER || {};
                 .enter()
                 .append('text')
                 .attr('x', function (d, i) {
-                    return 10 + i * 50;
+                    return d.x = 10 + i * 50;
                 })
                 .attr('y', function (d) {
-                    return d.depth * 50;
+                    return d.y = d.depth * 50;
                 })
                 .text(function (d) {
-                    return d.name || (_.isString(d.text) ? d.text.slice(0, 5) : 'null');
+                    return d.name || (_.isString(d.text) ? _.truncate(d.text, truncate_text_length) : 'null');
                 });
+
+            svg.selectAll('line')
+                .data(edges)
+                .enter()
+                .append('line')
+                .attr('x1', function (d) {
+                    return d.from.x;
+                })
+                .attr('y1', function (d) {
+                    return d.from.y;
+                })
+                .attr('x2', function (d) {
+                    return d.to.x;
+                })
+                .attr('y2', function (d) {
+                    return d.to.y;
+                })
+                .attr('stroke', 'blue')
+                .attr('stroke-width', 1);
         }
 
-        function create_node(ele, depth) {
+        function create_node(ele, depth, parent) {
+            var node = new ns.Node(ele, {
+                depth: depth
+            });
+            nodes.push(node);
+
+            if (parent !== undefined) {
+                edges.push({
+                    from: parent,
+                    to: node
+                });
+            }
+
             $(ele).contents().filter(function () {
                 // 要素ノードか非空文字テキストノードのみ許可
                 return _.contains([1, 3], this.nodeType) &&
                        !ns.util.isEmptyTextNode(this);
             }).each(function () {
-                nodes.push(new ns.Node(this, {
-                    depth: depth
-                }));
-
-                create_node(this, depth + 1);
+                create_node(this, depth + 1, node);
             });
         }
 
         that.init = function init(dom_text) {
-            dom = '<DomVisualizer>' + ns.util.replaceTagNames(dom_text) + '</DomVisualizer>';
+            dom = document.createElement('Root');
+            dom.innerHTML = ns.util.replaceTagNames(dom_text);
             edges = [];
             nodes = [];
             create_node(dom, 1);
